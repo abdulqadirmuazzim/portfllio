3fc3d35e7f7db6fb20172a9a6e76e9a4bc1dcb5f
abdulqadir <abdulqadirbinmuslim@gmail.com>
2024-04-10 01:32:49 +0100

    removed parallax scrolling due to issues, added scroll reveal animation, added progress bar and animated it

--- portfolio/templates/script.js
+++ portfolio/templates/script.js
@@ -12,20 +12,16 @@ checkbox.addEventListener('change', function () {
 
     // Change text & color of heading
     const head = document.querySelector("h2")
-    head.style.color = isChecked ? "#fff" : ""
+    head.style.color = isChecked ? "#000000" : ""
 
     const links = document.querySelectorAll("a")
     links.forEach(element => {
         element.style.color = isChecked ? "#fff" : ""
     });
 
-    // Change background color of container elements
-    const content = document.querySelectorAll('.container');
-    content.forEach(element => {
-        element.style.background = isChecked ? 'linear-gradient(45deg, #00264d, #000)' : '';
-        element.style.border = isChecked ? ".5px solid #fff" : ''
-        element.style.borderRadius = isChecked ? "10px" : ''
-    })
+    // Change background of home
+    const back_home = document.getElementById('backgound')
+    back_home.src = isChecked ? 'static/black.mp4' : ''
 
 
 })
@@ -45,3 +41,32 @@ window.addEventListener("scroll", () => {
     }
     value = window.scrollY
 });
+
+// animate on scroll effect for porgress bar
+
+// skills div
+const skills = document.querySelector(".skills")
+
+// progress bar fill
+const bar = document.querySelectorAll(".pro")
+
+window.addEventListener("scroll", () => {
+
+    const ele_pos = skills.getBoundingClientRect().y
+
+    const window_pos = window.innerHeight
+
+    if (ele_pos < window_pos) {
+        bar.forEach(element => {
+            element.style.width = '100%'
+            element.style.transition = '2.3s'
+            // element.style.animation = 'progress 2.5s 1'
+        })
+    }
+    else {
+        bar.forEach(element => {
+            element.style.width = '0%'
+            element.style.transition = '1.3s'
+        })
+    }
+})
